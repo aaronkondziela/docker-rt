4ea5b3770721d2d7700fc44bf8801c8b0db138eb
Aaron Kondziela <aaron@aaronkondziela.com>
2016-10-03 21:28:37 -0400

    prompt for last-stage image names

--- step3.sh
+++ step3.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 echo "Step 3: Building a docker-rt image based on the step 2 cpan image."
-docker build -f Dockerfile-3-rtsetup -t docker-rt .
+read -p "Image name [docker-rt]: " NAME
+if [ "$NAME" == "" ] ; then echo "Using default of docker-rt" ; NAME="docker-rt" ; fi
+
+docker build -f Dockerfile-3-rtsetup -t "$NAME" .
--- step4.sh
+++ step4.sh
@@ -4,6 +4,10 @@ RM="--rm"
 case "$YN" in
 	[Nn]* ) RM="" ;;
 esac
+
+read -p "What image name should we run? [docker-rt:latest]: " IMAGE
+if [ "$IMAGE" == "" ] ; then echo "Using default image name docker-rt:latest" ; IMAGE="docker-rt" ; fi
+
 echo "Go run /opt/docker-rt/setup.sh and use thisisatest when prompted to set passwords."
 echo "If all is well, rt should be available at localhost:80 on the host system."
-docker run -it -p 80:80 $RM -v /opt/rt-src:/opt/rt-src docker-rt:latest bash
+docker run -it -p 80:80 $RM -v /opt/rt-src:/opt/rt-src "$IMAGE" bash
