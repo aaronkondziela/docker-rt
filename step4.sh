21c6d0d0548b8147c4e3c46a8778c908e5497230
Aaron Kondziela <aaron@aaronkondziela.com>
2016-11-01 23:11:37 -0400

    added expect script for main setup

--- step4.sh
+++ step4.sh
@@ -19,6 +19,7 @@ esac
 read -p "What image name should we run? [docker-rt:latest]: " IMAGE
 if [ "$IMAGE" == "" ] ; then echo "Using default image name docker-rt:latest" ; IMAGE="docker-rt" ; fi
 
-echo "Go run /opt/docker-rt/setup.sh and use thisisatest when prompted to set passwords."
+echo "Go run /opt/docker-rt/init.expect"
+echo "   use thisisatest if prompted to set passwords."
 echo "If all is well, rt should be available at localhost:80 on the host system."
 docker run -it -p 80:80 $RM -v /opt/rt-src:/opt/rt-src -v /opt/rt-cx:/opt/rt-cx "$IMAGE" bash
