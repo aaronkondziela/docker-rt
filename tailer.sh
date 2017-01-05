47dd3a652cbd8a7e9fafdebb9ce1db3e6c70c68e
Aaron Kondziela <aaron@aaronkondziela.com>
2017-01-04 19:58:27 -0500

    tweak tailer so it doesn't fake me out

--- tailer.sh
+++ tailer.sh
@@ -1,1 +1,4 @@
-   tail -f /var/log/apache2/error.log | sed 's/\(critical\|error\|XXX\|warn\|syntax\)/'`printf "\033[33m"`'\1'`printf "\033[0m"`'/i'
+   echo "=========================================================================="
+   echo "|----> New tail started at `date`"
+   echo "|
+   tail -n0 -f /var/log/apache2/error.log | sed 's/\(critical\|error\|XXX\|warn\|syntax\)/'`printf "\033[33m"`'\1'`printf "\033[0m"`'/i'
