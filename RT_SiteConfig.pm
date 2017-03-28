6193c02cc3e1e3973c05e63563c14284be50bcfa
Aaron Kondziela <aaron@aaronkondziela.com>
2017-03-28 14:15:01 -0400

    Add StatementLog flag

--- RT_SiteConfig.pm
+++ RT_SiteConfig.pm
@@ -1,31 +1,13 @@
 use utf8;
 
-# Any configuration directives you include  here will override
-# RT's default configuration file, RT_Config.pm
-#
-# To include a directive here, just copy the equivalent statement
-# from RT_Config.pm and change the value. We've included a single
-# sample value below.
-#
-# If this file includes non-ASCII characters, it must be encoded in
-# UTF-8.
-#
-# This file is actually a perl module, so you can include valid
-# perl code, as well.
-#
-# The converse is also true, if this file isn't valid perl, you're
-# going to run into trouble. To check your SiteConfig file, use
-# this command:
-#
-#   perl -c /path/to/your/etc/RT_SiteConfig.pm
-#
-# You must restart your webserver after making changes to this file.
-
 Set( $rtname, 'example.com');
 Set( $OwnerEmail, 'joe_developer@example.com' );
 Set( $MailCommand, 'mbox' ); # no emails out to world; log to /opt/rt4/var/
+
 Set( $DevelMode, 1 ); # disables mason template/js/css caching
 Set( $LogToSTDERR, 'debug' ); # more debuggery
+Set( $StatementLog, 1 ); # Get SQL dumped
+
 Set( $CommentAddress, '' );
 Set( $CorrespondAddress, '' );
 Set( $DatabaseHost, 'localhost' );
@@ -40,9 +22,6 @@ Set( $WebDomain, 'localhost' );
 Set( $WebPort, '80' );
 Set( $RTAddressRegexp, 'localhost|127\.0\.0\.1' );
 
-# You must install Plugins on your own, this is only an example
-# of the correct syntax to use when activating them:
 #     Plugin( "RT::Extension::SLA" );
-#     Plugin( "RT::Authen::ExternalAuth" );
 
 1;
