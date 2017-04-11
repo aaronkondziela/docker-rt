8ff4b02cb1791c8f18df910d5f247fcbb5612a1f
Aaron Kondziela <aaron@aaronkondziela.com>
2017-04-10 21:48:00 -0400

    tailer regex tweaks

--- tailer.sh
+++ tailer.sh
@@ -1,4 +1,4 @@
-   echo "=========================================================================="
-   echo "|----> New tail started at `date`"
-   echo "|"
-   tail -n0 -f /var/log/apache2/error.log | sed 's/\(critical\|error\|XXX\|warn\|syntax\)/'`printf "\033[33m"`'\1'`printf "\033[0m"`'/i'
+echo "=========================================================================="
+echo "|----> New tail started at `date`"
+echo "|"
+tail -n0 -f /var/log/apache2/error.log | sed 's/\(\S\+:[0-9]\+)$\|\S\+ line [0-9]\+\.$\|critical\|error\|XXX\|warn\|syntax\)/'`printf "\033[33m"`'\1'`printf "\033[0m"`'/i'
